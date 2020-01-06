ce537e007746144588f1ea80a572ebd2cda3cbf1
Peter Han <peterjhan88@gmail.com>
2020-01-06 12:43:56 -0800

    State is completely removed. Cahnged to direct referral to values of props

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
 class Grade extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: props.id,
-      name: props.name,
-      course: props.course,
-      grade: props.grade
-    };
-  }
-
   render() {
     return (
       <tr>
-        <td className='col-xs-3'>{this.state.name}</td>
-        <td className='col-xs-3'>{this.state.course}</td>
-        <td className='col-xs-2'>{this.state.grade}</td>
+        <td className='col-xs-3'>{this.props.name}</td>
+        <td className='col-xs-3'>{this.props.course}</td>
+        <td className='col-xs-2'>{this.props.grade}</td>
       </tr>
     );
   }
