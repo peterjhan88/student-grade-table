a56b1458d655fd61ab029cf0196301990a85a293
Peter Han <peterjhan88@gmail.com>
2020-01-07 17:38:04 -0800

    Completed update-a-grade | Peter Han Also, changed button size in grade-form. Also, changed cancel button color.'

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,10 +6,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: []
+      grades: [],
+      updateTarget: null
     };
     this.addInputs = this.addInputs.bind(this);
     this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleUpdateButtonClick = this.handleUpdateButtonClick.bind(this);
+    this.handleCancelButtonClick = this.handleCancelButtonClick.bind(this);
   }
 
   Header(props) {
@@ -25,22 +28,52 @@ class App extends React.Component {
   }
 
   addInputs(newGrade) {
-    fetch('/api/grades/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newGrade)
-    })
-      .then(response => response.json())
-      .then(jsonData => {
-        this.setState(previousState => {
-          var newGrades = previousState.grades;
-          newGrades.push(jsonData);
-          return { grades: newGrades };
-        });
+    const isModeAdd = this.state.updateTarget === null;
+    if (isModeAdd) {
+      fetch('/api/grades/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newGrade)
       })
-      .catch(error => {
-        console.error(error.message);
-      });
+        .then(response => response.json())
+        .then(jsonData => {
+          this.setState(previousState => {
+            var newGrades = previousState.grades;
+            newGrades.push(jsonData);
+            return {
+              grades: newGrades
+            };
+          });
+        })
+        .catch(error => {
+          console.error(error.message);
+        });
+    } else {
+      const { targetId } = newGrade;
+      const body = {
+        name: newGrade.name,
+        course: newGrade.course,
+        grade: newGrade.grade
+      };
+      fetch(`/api/grades/${targetId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+        .then(response => response.json())
+        .then(updatedGrade => {
+          this.setState(previousState => {
+            var newGrades = previousState.grades.map(grade => grade.id === updatedGrade.id ? updatedGrade : grade);
+            return {
+              grades: newGrades,
+              updateTarget: null
+            };
+          });
+        })
+        .catch(error => {
+          console.error(error.message);
+        });
+    }
   }
 
   deleteGrade(targetId) {
@@ -56,12 +89,24 @@ class App extends React.Component {
               newGrades.splice(index, 1);
             }
           }
-          return { grades: newGrades };
+          return {
+            grades: newGrades,
+            updateTarget: null
+          };
         });
       })
       .catch(error => {
         console.error(error.message);
       });
+  }
+
+  handleCancelButtonClick() {
+    this.setState({ updateTarget: null });
+  }
+
+  handleUpdateButtonClick(targetId) {
+    const [updateTarget] = this.state.grades.filter(grade => grade.id === targetId);
+    this.setState({ updateTarget: updateTarget });
   }
 
   componentDidMount() {
@@ -94,8 +139,17 @@ class App extends React.Component {
       <>
         <this.Header titleText='Student Grade Table' averageGrade={this.getAverageGrade()}/>
         <div className='d-flex flex-wrap'>
-          <GradeTable grades={this.state.grades} handleDeleteButtonClick={this.deleteGrade}/>
-          <GradeForm onSubmit={this.addInputs} />
+          <GradeTable
+            grades={this.state.grades}
+            handleDeleteButtonClick={this.deleteGrade}
+            handleUpdateButtonClick={this.handleUpdateButtonClick}
+          />
+          <GradeForm
+            onSubmit={this.addInputs}
+            mode={this.state.updateTarget ? 'Update' : 'Add'}
+            updateTarget={this.state.updateTarget}
+            handleCancelButtonClick={this.handleCancelButtonClick}
+          />
         </div>
       </>
     );
--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -4,6 +4,7 @@ class GradeForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'Add',
       name: '',
       course: '',
       grade: ''
@@ -20,6 +21,9 @@ class GradeForm extends React.Component {
       course: this.state.course,
       grade: parseInt(this.state.grade)
     };
+    if (this.state.mode === 'Update') {
+      newGrade.targetId = this.props.updateTarget.id;
+    }
     this.props.onSubmit(newGrade);
     this.inputClear();
   }
@@ -34,13 +38,26 @@ class GradeForm extends React.Component {
     var clearInputs = {
       name: '',
       course: '',
-      grade: ''
+      grade: '',
+      mode: 'Add'
     };
     this.setState(clearInputs);
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.mode !== prevProps.mode && this.props.mode !== 'Add') {
+      this.setState({
+        mode: this.props.mode,
+        name: this.props.updateTarget.name,
+        course: this.props.updateTarget.course,
+        grade: this.props.updateTarget.grade
+      });
+    }
+  }
+
   handleCancel(event) {
     event.preventDefault();
+    this.props.handleCancelButtonClick();
     this.inputClear();
   }
 
@@ -59,9 +76,9 @@ class GradeForm extends React.Component {
           <label className='col-3 col-form-label-sm d-flex justify-content-end align-items-center'><i className='fas fa-graduation-cap'></i></label>
           <input className='col-8 form-control' type='text' id='grade' onChange={this.handleChange} value={this.state.grade} placeholder='Grade' />
         </div>
-        <div className='d-flex flex-nowrap row col-8 justify-content-between'>
-          <button id='add' type='submit' className='btn btn-success col-5'>Add</button>
-          <button id='cancel' className='btn btn-warning col-5' onClick={this.handleCancel} >Cancel</button>
+        <div className='d-flex flex-nowrap row col-9 justify-content-between'>
+          <button id='add' type='submit' className={this.state.mode === 'Add' ? 'btn col-5 btn-success' : 'btn col-5 btn-warning'}>{this.state.mode}</button>
+          <button id='cancel' className='btn btn-light border border-dark col-5' onClick={this.handleCancel} >Cancel</button>
         </div>
       </form>
     );
