1f1ca565bb3edbf5e4d24880c1069df3693d87f2
Peter Han <peterjhan88@gmail.com>
2020-01-08 13:19:55 -0800

    Completed new server - GET all and single grade

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
-const path = require('path');
-const jsonServer = require('json-server');
+const dataPath = '../database/db.json';
+const express = require('express');
+const app = express();
+var dataOnServer = require(dataPath);
+var errorObject = {};
+var serverDate = new Date();
 
-const dbPath = path.resolve(__dirname, '../database/db.json');
-const server = jsonServer.create();
-const middleware = jsonServer.defaults();
-const endpoints = jsonServer.router(dbPath);
-
-server.use(middleware);
-server.use('/api', endpoints);
-server.listen(3001, () => {
+app.get('/api/grades', (req, res) => {
+  const data = dataOnServer.grades;
+  res.status(200).send(data);
   // eslint-disable-next-line no-console
-  console.log('JSON Server listening on port 3001\n');
+  console.log(req.method, 'All Grades', serverDate.toLocaleTimeString());
 });
+
+app.get('/api/grades/:id', (req, res) => {
+  const targetId = req.params.id;
+  const data = dataOnServer.grades;
+  const grade = gradeWithTargetId(data, targetId);
+  if (!isIdValid(targetId)) {
+    errorObject.error = 'id must be positive integer';
+    res.status(400).send(errorObject);
+    return false;
+  } else if (!grade) {
+    errorObject.error = 'id does not exist in data';
+    res.status(404).send(errorObject);
+    return false;
+  }
+  res.status(200).send(grade);
+  // eslint-disable-next-line no-console
+  console.log(req.method, `id=${req.params.id}`, serverDate.toLocaleTimeString());
+});
+
+app.listen(3000, () => {
+// eslint-disable-next-line no-console
+  console.log('Server initiated. Listening on port 3000..');
+});
+
+const isIdValid = id => (parseInt(id) > 0) && (!id.match(/\D/i));
+const gradeWithTargetId = (data, id) => data.find(grade => grade.id === parseInt(id)) || false;
