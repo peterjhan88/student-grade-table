839bca6ea8373bd77a8244f252416754623971df
Peter Han <peterjhan88@gmail.com>
2020-02-10 13:01:48 -0800

    deleted unnecessary files package.json changed to have required packages to make this project fullstack dummy database in json deleted folder structure changed to fit program structure

--- server/index.js
+++ server/index.js
@@ -1,193 +1,40 @@
 /* eslint-disable no-console */
+require('dotenv/config');
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const portNumber = 3001;
-const dataPath = path.resolve(__dirname, '../database/data.json');
+
+const db = require('./database');
+const ClientError = require('./client-error');
+const staticMiddleware = require('./static-middleware');
+const sessionMiddleware = require('./session-middleware');
+
 const app = express();
-const jsonParserMiddleWare = express.json();
-var dataOnServer = require(dataPath);
-var serverDate = new Date();
 
-app.get('/api/grades', (req, res) => {
-  const data = dataOnServer.grades;
-  res.status(200).send(data);
-  console.log(req.method, 'All Grades', serverDate.toLocaleTimeString());
+app.use(staticMiddleware);
+app.use(sessionMiddleware);
+
+app.use(express.json());
+
+app.get('/api/health-check', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
 });
 
-app.get('/api/grades/:id', (req, res) => {
-  const targetId = req.params.id;
-  if (invalidIdHandle(targetId, res)) {
-    return false;
-  }
-  const grade = dataOnServer.grades.find(grade => grade.id === parseInt(targetId));
-  res.status(200).send(grade);
-  console.log(req.method, `id=${req.params.id}`, serverDate.toLocaleTimeString());
+app.use('/api', (req, res, next) => {
+  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
 
-app.use(jsonParserMiddleWare);
-app.post('/api/grades', (req, res) => {
-  var newGrade = req.body;
-  if (!checkBody(newGrade)) {
-    const errorObject = validateGrade(newGrade);
-    res.status(400).send(errorObject);
-    return false;
+app.use((err, req, res, next) => {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message });
   } else {
-    newGrade.id = dataOnServer.nextId++;
-    dataOnServer.grades.push(newGrade);
-    const stringifiedUpdatedData = JSON.stringify(dataOnServer, null, 2);
-    fs.writeFile(dataPath, stringifiedUpdatedData, fsWriteFileError => {
-      if (fsWriteFileError) {
-        const errorObject = {
-          error: 'Unexpected error occurred.'
-        };
-        res.status(500).send(errorObject);
-        return false;
-      }
-      res.status(201).send(newGrade);
-      console.log(req.method, serverDate.toLocaleTimeString());
-      return true;
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
     });
   }
 });
 
-app.delete('/api/grades/:id', (req, res) => {
-  const targetId = req.params.id;
-  if (isIdValid(targetId) && gradeWithTargetId(targetId)) {
-    const targetIndex = dataOnServer.grades.findIndex(grade => grade.id === parseInt(targetId));
-    dataOnServer.grades.splice(targetIndex, 1);
-    const stringifiedUpdatedData = JSON.stringify(dataOnServer, null, 2);
-    fs.writeFile(dataPath, stringifiedUpdatedData, fsWriteFileError => {
-      if (fsWriteFileError) {
-        const errorObject = {
-          error: 'Unexpected error occurred.'
-        };
-        res.status(500).send(errorObject);
-        return false;
-      }
-      res.sendStatus(204);
-      console.log(req.method, serverDate.toLocaleTimeString());
-      return true;
-    });
-  } else {
-    if (invalidIdHandle(targetId, res)) {
-      return false;
-    }
-  }
+app.listen(process.env.PORT, () => {
+  console.log(`Server initiated. Listening on port ${process.env.PORT}...\n`);
 });
-
-app.put('/api/grades/:id', (req, res) => {
-  const targetId = req.params.id;
-  const reqBody = req.body;
-  if (isIdValid(targetId) && gradeWithTargetId(targetId) && checkBody(reqBody)) {
-    const targetIndex = dataOnServer.grades.findIndex(grade => grade.id === parseInt(targetId));
-    reqBody.id = parseInt(targetId);
-    dataOnServer.grades.splice(targetIndex, 1, reqBody);
-    const stringifiedUpdatedData = JSON.stringify(dataOnServer, null, 2);
-    fs.writeFile(dataPath, stringifiedUpdatedData, fsWriteFileError => {
-      if (fsWriteFileError) {
-        const errorObject = {
-          error: 'Unexpected error occurred.'
-        };
-        res.status(500).send(errorObject);
-        return false;
-      }
-      res.status(200).send(reqBody);
-      console.log(req.method, serverDate.toLocaleTimeString());
-      return true;
-    });
-  } else {
-    if (invalidIdHandle(targetId, res)) {
-      return false;
-    } else {
-      const errorObject = validateGrade(reqBody);
-      res.status(400).send(errorObject);
-      return false;
-    }
-  }
-});
-
-app.patch('/api/grades/:id', (req, res) => {
-  const targetId = req.params.id;
-  const reqBody = req.body;
-  if (isIdValid(targetId) && gradeWithTargetId(targetId)) {
-    const targetIndex = dataOnServer.grades.findIndex(grade => grade.id === parseInt(targetId));
-    reqBody.id = parseInt(targetId);
-    var originalData = dataOnServer.grades[targetIndex];
-    for (var key in originalData) {
-      if (!Object.keys(reqBody).includes(key)) {
-        reqBody[key] = originalData[key];
-      }
-    }
-    dataOnServer.grades.splice(targetIndex, 1, reqBody);
-    const stringifiedUpdatedData = JSON.stringify(dataOnServer, null, 2);
-    fs.writeFile(dataPath, stringifiedUpdatedData, fsWriteFileError => {
-      if (fsWriteFileError) {
-        const errorObject = {
-          error: 'Unexpected error occurred.'
-        };
-        res.status(500).send(errorObject);
-        return false;
-      }
-      res.status(200).send(reqBody);
-      console.log(req.method, serverDate.toLocaleTimeString());
-      return true;
-    });
-  } else {
-    if (invalidIdHandle(targetId, res)) {
-      return false;
-    }
-  }
-});
-
-app.listen(portNumber, () => {
-  console.log(`Server initiated. Listening on port ${portNumber}...\n`);
-});
-
-function isIdValid(id) {
-  return parseInt(id) > 0 && !id.match(/\D/i);
-}
-
-function gradeWithTargetId(id) {
-  if (dataOnServer.grades.find(grade => grade.id === parseInt(id))) {
-    return true;
-  }
-  return false;
-}
-
-function checkBody(newGrade) {
-  if (!newGrade.name || !newGrade.course || !newGrade.grade) {
-    return false;
-  }
-  return true;
-}
-
-function validateGrade(gradeData) {
-  const errors = {};
-  const requiredFields = ['name', 'course', 'grade'];
-  requiredFields.forEach(field => {
-    if (!gradeData[field]) {
-      errors[field] = `${field} is a required field`;
-    }
-  });
-  if (Object.keys(errors).length > 0) {
-    return errors;
-  }
-  return null;
-}
-
-function invalidIdHandle(id, res) {
-  if (!isIdValid(id)) {
-    const errorObject = {
-      error: 'id must be positive integer'
-    };
-    res.status(400).send(errorObject);
-    return true;
-  } else if (!gradeWithTargetId(id)) {
-    const errorObject = {
-      error: 'id does not exist in data'
-    };
-    res.status(404).send(errorObject);
-    return true;
-  }
-}
